6157773cbdf0b4c4c752e8d6cacec9a1a9aa15a1
Michael Bedward <michael.bedward@gmail.com>
2019-04-05 19:22:29 +1100

    Allow prepare_tile to skip normalizing heights. Better orientation labelling for flight lines. Guard against flight lines with few points.

--- R/main_functions.R
+++ R/main_functions.R
@@ -12,9 +12,15 @@
 #'   file extension) although it can also contain other files (e.g. HTML or XML
 #'   documents).
 #'
+#' @param normalize.heights If TRUE (default), normalize all point heights to
+#'   ground level as estimated from an elevation surface fitted by Delaunay
+#'   triangulation. The original point heights are kept as a new data table
+#'   column 'Zref'.
+#'
 #' @param drop.negative If TRUE, any points (other than ground points) whose
 #'   heights are below ground level (as estimated by Delaunay interpolation of
-#'   ground point heights) are discarded.
+#'   ground point heights) are discarded. This argument only applies if
+#'   \code{normalize.heights = TRUE}.
 #'
 #' @param fields A string containing single-letter abbreviations for the data
 #'   fields to include. The default is to return all fields. See
@@ -34,11 +40,10 @@
 #'
 #' @return A \code{LAS} object.
 #'
-#' @importFrom dplyr %>%
-#'
 #' @export
 #'
 prepare_tile <- function(path,
+                         normalize.heights = TRUE,
                          drop.negative = TRUE,
                          fields = "*",
                          classes = c(2, 3, 4, 5, 6),
@@ -70,20 +75,22 @@ prepare_tile <- function(path,
 
   filtertxt <- paste("-keep_class", paste(classes, collapse = " "))
 
-  las <- lidR::readLAS(las.file, select = fields, filter = filtertxt) %>%
-    # Normalize point heights relative to ground level
-    lidR::lasnormalize(algorithm = lidR::tin()) %>%
+  las <- lidR::readLAS(las.file, select = fields, filter = filtertxt)
 
-    # Add flight line indices based on GPS times for points
-    #lidR::lasflightline(dt = flight.gap)
-    .tag_flight_line(., dt = flight.gap)
+  # Normalize point heights relative to ground level
+  if (normalize.heights) {
+    las <- lidR::lasnormalize(las, algorithm = lidR::tin())
 
-
-  # remove negative heights
-  if (drop.negative) {
-    ii <- las@data$Classification == 2 | las@data$Z > 0
-    las@data <- las@data[ ii, ]
+    # remove negative heights
+    if (drop.negative) {
+      ii <- las@data$Classification == 2 | las@data$Z > 0
+      las@data <- las@data[ ii, ]
+    }
   }
+
+  # Add flight line indices based on GPS times for points
+  #lidR::lasflightline(dt = flight.gap)
+  las <- .tag_flight_line(las, dt = flight.gap)
 
   if (zipped) {
     unlink(unz.files)
@@ -119,56 +126,117 @@ prepare_tile <- function(path,
 #' Check that the orientations of all flightlines match
 #'
 #' This is a convenience function that calls \code{\link{get_flightline_dir}}
-#' and checks that all flight lines have the same orientation and are either
-#' E-W or N-S.
+#' with default arguments and checks that all flight lines have the same
+#' orientation, either 'EW' or 'NS'.
 #'
 #' @param las A LAS object, e.g. imported using \code{\link{prepare_tile}}.
 #'
-#' @param ratio Value passed to \code{get_flightline_dir} for the minimum ratio
-#'   of X and Y dimensions to be classified as either 'EW' or 'NS'.
-#'
 #' @return \code{TRUE} if flight lines are consistent; \code{FALSE} otherwise.
 #'
-#' @importFrom dplyr %>% group_by mutate summarize
-#'
-#' @seealso \code{\link{get_flightline_dir}}
+#' @seealso \code{\link{get_flightline_info}}
 #'
 #' @export
 #'
-check_flightlines <- function(las, ratio = 1.5) {
+check_flightlines <- function(las) {
   dat <- get_flightline_info(las)
-  o <- dat$orientation
+  o <- na.omit(dat$orientation)
 
-  (o[1] %in% c("EW", "NS")) && all(o == o[1])
+  if (length(o) == 0) FALSE
+  else (o[1] %in% c("EW", "NS")) && all(o == o[1])
 }
 
 
 #' Get summary information for flight line extents
 #'
-#' This function determines the minimum bounding rectangle of points in each flight line
-#' and, based on this, the orientation: one of NS (north - south), EW (east - west),
-#' or XX (indeterminate).
+#' This function determines the minimum bounding rectangle for points in each
+#' flight line and, based on this, the orientation of the flight line: one of
+#' 'NS' (north - south), 'EW' (east - west), or XX (indeterminate).
+#'
+#' To determine orientation, the function first checks the angle between the
+#' longest side of the rectangle and the horizontal (X coordinate axis). If the
+#' longest side is close to horizontal (where 'close' means plus or minus a
+#' tolerance value specified with the \code{angular.tol} argument which has a
+#' default value of 30 degrees) the orientation is provisionally labelled as
+#' 'EW', whereas if the longest side is close to vertical (Y coordinate axis) it
+#' is provisionally labelled as 'NS'. Flight lines that are not sufficiently
+#' close to horizontal or vertical are labelled as 'XX' and not considered
+#' further.
+#'
+#' For flight lines labelled 'NS' or 'EW', if the ratio of the longest side of
+#' the bounding rectangle to the shortest side is greater than that specified by
+#' the \code{ratio.side} argument (default 1.2), the initial orientation is
+#' taken as confirmed. For flight lines with more equilateral bounding
+#' rectangles, the point GPS times are checked along the X and Y dimensions to
+#' determine the final orientation.
+#'
 #'
 #' @param las A LAS object, e.g. imported using \code{\link{prepare_tile}}.
 #'
-#' @param ratio Minimum ratio of X and Y dimensions to be classified as either
-#'   E-W or N-S. Flight lines that do not satisfy this ratio are classified
-#'   as 'XX'.
+#' @param angular.tol The angular tolerance in degrees to apply when determining
+#'   flight line orientation. It refers to the angle between the longest side of
+#'   bounding rectangle and the horizontal (X coordinate axis). The default
+#'   value is 15 degrees and the valid range of values is
+#'   \code{0 < angular.tol <= 30}.
+#'
+#' @param ratio.min The minimum ratio (default = 1.2) of the longest to the
+#'   shortest side of a flight line bounding rectangle for orientation to be
+#'   either 'NS' or 'EW'. Must be a value greater than 1.1. For flight lines
+#'   with more equilateral bounding rectangles, orientation is determined by
+#'   examining point GPS times (see Details).
+#'
+#' @param npoints.min The minimum number of points in a flight line for it to
+#'   be included. Set to zero to include all flightlines. The default value
+#'   (1e4) is intended to exclude flight lines that only appear at the margins
+#'   of the tile. In the case that all flight lines are included, bounding
+#'   rectangle and orientations will not be defined for those with very few
+#'   points.
 #'
 #' @return A spatial data frame (class \code{sf}) with columns: flightlineID,
 #'   xlen, ylen, orientation and geometry (minimum bounding rectangle of flight
 #'   line).
 #'
-#' @importFrom dplyr %>% case_when group_by left_join mutate summarize
+#' @importFrom dplyr %>%
 #'
 #' @seealso \code{\link{check_flightlines}}
 #'
 #' @export
 #'
-get_flightline_info <- function(las, ratio = 1.5) {
-  stopifnot(ratio > 1)
+get_flightline_info <- function(las,
+                                angular.tol = 15,
+                                ratio.min = 1.2,
+                                npoints.min = 1e4) {
+
+  stopifnot(angular.tol > 0.0, angular.tol <= 30)
+  stopifnot(ratio.min > 1.1)
 
   ids <- sort(unique(las@data$flightlineID))
+
+  # Count points in each flight line.
+  # The c() call is to convert the table object to a
+  # simple vector.
+  counts.all <- c( table(las@data$flightlineID) )
+  total.count <- sum(counts.all)
+
+  # Check for flightlines with an insufficient number of points
+  n <- sum(counts.all < npoints.min)
+  if (n == 0) {
+    counts <- counts.all
+    ids.fewpoints <- integer(0)
+
+  } else {
+    ids.fewpoints <- ids[c(counts.all) < npoints.min]
+    ids <- setdiff(ids, ids.fewpoints)
+
+    if (length(ids) == 0) stop("No flight lines with sufficient points")
+
+    counts <- counts.all[ as.character(ids) ]
+  }
+
+  counts <- data.frame(
+    flightlineID = ids,
+    npoints = counts
+  )
+
 
   rects <- lapply(ids, function(id) {
     r <- get_bounding_rectangle(las, classes = "all", flightlines = id)
@@ -184,19 +252,117 @@ get_flightline_info <- function(las, ratio = 1.5) {
 
   sfdat <- sf::st_sf(flightlineID = ids, geometry = geoms)
 
-  rects <- dplyr::bind_rows(rects) %>%
-    group_by(flightlineID) %>%
 
-    summarize(xlen = diff(range(X)),
-              ylen = diff(range(Y))) %>%
+  rects <- bind_rows(rects) %>%
+    dplyr::left_join(counts, by = "flightlineID") %>%
 
-    mutate(orientation = case_when(
-      xlen > ratio * ylen ~ "EW",
-      ylen > ratio * xlen ~ "NS",
-      TRUE ~ "XX"
-    ))
+    dplyr::group_by(flightlineID) %>%
 
-  left_join(rects, sfdat, by = "flightlineID")
+    dplyr::do({
+      # Find side lengths and angle of longest side
+      dxs <- diff(.$X[1:3])
+      dys <- diff(.$Y[1:3])
+
+      side12.len <- sqrt(dxs[1]^2 + dys[1]^2)
+      side23.len <- sqrt(dxs[2]^2 + dys[2]^2)
+
+      side.angle <- ifelse(
+        side12.len > side23.len, atan2(dys[1], dxs[1]), atan2(dys[2], dxs[2])
+      )
+
+      # convert to degrees and express as angle to horizontal
+      side.angle <- 180 * side.angle / pi
+      if (side.angle < 0) side.angle <- side.angle + 180
+      if (side.angle > 90) side.angle <- 180 - side.angle
+
+      # Initially classify orientation
+      o <- base::cut(
+        side.angle,
+        breaks = c(-1, angular.tol, 90 - angular.tol, 91),
+        labels = c("EW", "XX", "NS"))
+
+      # Convert to character to avoid factor level problems later
+      orientation.initial <- as.character(o)
+
+      # Side length ratio
+      lens <- c(side12.len, side23.len)
+      ratio.sidelen <- max(lens) / min(lens)
+
+      data.frame(angle = side.angle, orientation.initial, ratio.sidelen,
+                 stringsAsFactors = FALSE)
+    }) %>%
+
+    dplyr::ungroup()
+
+
+  # GPS point time trend along X and Y dimensions for each flight line
+  # and orientation inferred from this
+  timetrend <- las@data %>%
+    dplyr::filter(flightlineID %in% ids) %>%
+
+    dplyr::select(X, Y, flightlineID, gpstime) %>%
+
+    dplyr::group_by(flightlineID) %>%
+
+    dplyr::mutate(dtime = gpstime - min(gpstime)) %>%
+
+    dplyr::sample_n(min(1000, n())) %>%
+
+    dplyr::do({
+      model <- lm(dtime ~ X + Y, data = .)
+      pdat <- expand.grid(X = range(.$X), Y = range(.$Y)) %>% dplyr::arrange(X, Y)
+      p <- predict(model, newdata = pdat)
+      dtX <- abs(p[1] - p[3])
+      dtY <- abs(p[1] - p[2])
+
+      otime <- ifelse(dtY > 10 * dtX, "NS", ifelse(dtX > 100 * dtY, "EW", "XX"))
+
+      data.frame(dtX, dtY, orientation.time = otime,
+                 stringsAsFactors = FALSE)
+    })
+
+
+  rects <- dplyr::left_join(rects, timetrend,
+                            by = "flightlineID") %>%
+
+    dplyr::mutate(orientation = dplyr::case_when(
+      orientation.initial == "XX" ~ orientation.initial,
+      ratio.sidelen > ratio.min ~ orientation.initial,
+      TRUE ~ orientation.time
+    )) %>%
+
+    dplyr::select(flightlineID, angle, ratio.sidelen, orientation)
+
+
+  sfdat <- sfdat %>%
+    dplyr::left_join(rects, by = "flightlineID") %>%
+    dplyr::left_join(counts, by = "flightlineID") %>%
+    dplyr::mutate(ppoints = npoints / total.count)
+
+  if (length(ids.fewpoints) > 0) {
+    npoints.few <- counts.all[ as.character(ids.fewpoints) ]
+    ppoints.few <- npoints.few / total.count
+
+    suppressWarnings(
+      sfdat.few <- sf::st_sf(
+        flightlineID = ids.fewpoints,
+        angle = NA_real_,
+        ratio.sidelen = NA_real_,
+        orientation = NA_character_,
+        npoints = npoints.few,
+        ppoints = ppoints.few,
+        geometry = sf::st_sfc( sf::st_polygon() ),
+
+        stringsAsFactors = FALSE)
+    )
+
+    # Note: using dplyr::rbind does not work here for some reason
+    # (with dplyr 0.8.0.1)
+    sfdat <- rbind(sfdat, sfdat.few) %>%
+      dplyr::arrange(flightlineID)
+  }
+
+  sfdat
 }
 
 
@@ -213,7 +379,7 @@ get_flightline_info <- function(las, ratio = 1.5) {
 #' @param classes Point classes to include: either a vector of integer class
 #'   numbers or the string \code{'all'} (default).
 #'
-#' @param flightlineIDs Flight lines to include: either a vector of integer
+#' @param flightlines Flight lines to include: either a vector of integer
 #'   class numbers or the string \code{'all'} (default).
 #'
 #' @return A data frame of corner vertices for the minimum bounding rectangle.
@@ -232,7 +398,7 @@ get_bounding_rectangle <- function(las, classes = "all", flightlines = "all") {
     if (tolower(flightlines) == "all") {
       flightlines <- lines.all
     } else if (length(flightlines) < 1) {
-      stop("Argument flightlineIDs should either be 'all' or a vector of one or more integer values")
+      stop("Argument flightlines should either be 'all' or a vector of one or more integer values")
     } else {
       ii <- flightlines %in% lines.all
       if (!all(ii)) {
@@ -290,12 +456,17 @@ get_bounding_rectangle <- function(las, classes = "all", flightlines = "all") {
 #' @export
 #'
 .min_rectangle <- function(X, Y = NULL) {
-  xy <- xy.coords(X, Y)
+  xy <- grDevices::xy.coords(X, Y)
 
   # find vertices of the convex hull
   xy <- cbind(xy$x, xy$y)
   ii <- grDevices::chull(xy)
   m <- xy[ii, ]
+
+  if (nrow(m) < 3) {
+    warning("Minimum rectangle is undefined (e.g. single point or collinear points)")
+    return(data.frame(X = NA_real_, Y = NA_real_))
+  }
 
   # edge lengths
   dm <- cbind(
@@ -349,13 +520,21 @@ get_bounding_rectangle <- function(las, classes = "all", flightlines = "all") {
 #' @param buffer Width of a buffer placed around the tile to ensure that the
 #'   boundaries partitioning overlap areas extend beyond all points.
 #'
+#' @param npoints.min The minimum number of points in a flight line for it to be
+#'   considered. Flight lines with fewer points are discarded. The default value
+#'   (1e4) is intended to exclude flight lines that only appear at the margins
+#'   of the tile.
+#'
 #' @return A modified copy of the input LAS object.
 #'
-#' @seealso \code{\link{check_flightlines}}, \code{\link{plot_flightlines}}
+#' @seealso \code{\link{get_flightline_info}}, \code{\link{check_flightlines}},
+#'   \code{\link{plot_flightlines}}
+#'
+#' @importFrom dplyr %>%
 #'
 #' @export
 #'
-remove_flightline_overlap <- function(las, classes = 5, res = 10, buffer = 100) {
+remove_flightline_overlap <- function(las, classes = 5, res = 10, buffer = 100, npoints.min = 1e4) {
   flines <- sort(unique(las@data$flightlineID))
   nlines <- length(flines)
 
@@ -366,8 +545,32 @@ remove_flightline_overlap <- function(las, classes = 5, res = 10, buffer = 100) 
 
   # Get flight line info (bounding rectangles etc) and check for
   # consistent orientation
-  fline.dat <- get_flightline_info(las)
-  o <- fline.dat$orientation
+  fline.dat <- get_flightline_info(las, npoints.min = npoints.min)
+
+  # Check that each flight line has sufficient points
+  fline.dat <- fline.dat %>%
+    mutate(included = npoints >= npoints.min)
+
+  if (!any(fline.dat$included)) {
+    stop("No flight lines have sufficient points.")
+  }
+
+  # Remove points for flight lines with too few points
+  ids <- fline.dat$flightlineID[ fline.dat$included ]
+  ii <- las@data$flightlineID %in% ids
+  las@data <- las@data[ii, ]
+
+  fline.dat <- dplyr::filter(fline.dat, included)
+
+  if (nrow(fline.dat) == 1) {
+    message("Only one flight line with sufficient points was retained.")
+    return(las)
+  }
+
+
+  # At least two flight lines have sufficient points...
+  #
+  o <- fline.dat$orientation[ fline.dat$included ]
   ok <- (o[1] %in% c("NS", "EW")) && all(o == o[1])
   if (!ok) {
     warning("Flight lines have inconsistent orientation. Cannot check overlaps.")
@@ -576,7 +779,7 @@ plot_flightlines <- function(las, npts = 5000, shape = 16, size = 1) {
 #'
 #' @return A data frame of start and end times.
 #'
-#' @importFrom dplyr %>% group_by summarize arrange
+#' @importFrom dplyr %>%
 #' @export
 #'
 get_scan_times <- function(las, by) {
@@ -591,11 +794,12 @@ get_scan_times <- function(las, by) {
 
   if (by == "all") {
     data.frame(time.start = min(times), time.end = max(times))
+
   } else {
     data.frame(flightlineID = las@data$flightlineID, times) %>%
-      group_by(flightlineID) %>%
-      summarize(time.start = min(times), time.end = max(times)) %>%
-      arrange(flightlineID)
+      dplyr::group_by(flightlineID) %>%
+      dplyr::summarize(time.start = min(times), time.end = max(times)) %>%
+      dplyr::arrange(flightlineID)
   }
 }
 
@@ -638,7 +842,8 @@ is_prepared_tile <- function(las) {
 get_stratum_counts <- function(las, strata, res = 10, classes = c(2,3,4,5)) {
 
   if (!is_prepared_tile(las))
-    stop("Argument 'las' must be a LAS object imported with prepare_tile")
+    stop("Argument 'las' must be a LAS object imported with prepare_tile\n",
+         "  and with point heights normalized to ground level")
 
   strata <- check_strata(strata)
 
@@ -717,7 +922,6 @@ get_stratum_counts <- function(las, strata, res = 10, classes = c(2,3,4,5)) {
 #' }
 #'
 #' @importFrom dplyr %>%
-#' @importFrom sf st_as_sf
 #'
 #' @export
 #'
@@ -730,7 +934,7 @@ get_building_points <- function(las) {
     dplyr::filter(Classification == 6) %>%
     dplyr::select(X, Y, Z)
 
-  st_as_sf(dat, coords = c("X", "Y"))
+  sf::st_as_sf(dat, coords = c("X", "Y"))
 }
 
 
@@ -769,7 +973,7 @@ get_stratum_cover <- function(rcounts) {
 
 #' Metrics function for summary statistics
 #'
-#' @importFrom dplyr %>% group_by
+#' @importFrom dplyr %>%
 #'
 #' @export
 #'
@@ -784,8 +988,9 @@ layer_summary_metrics <- function(z, prob, breaks) {
 
   dat <- data.frame(z, zcat)
 
-  x <- dat %>% group_by(zcat) %>%
-    summarize(n = n(),
+  x <- dat %>%
+    dplyr::group_by(zcat) %>%
+    dplyr::summarize(n = n(),
               mean = mean(z),
               median = median(z),
               lwr = highest_density_interval(z, 0.5)[1],
