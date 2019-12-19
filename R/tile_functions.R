25624d3d5b4fd708d05b9edf58c966b58b42153e
Michael Bedward <michael.bedward@gmail.com>
2019-12-19 17:01:37 +1100

    relate_strata function

--- R/tile_functions.R
+++ R/tile_functions.R
@@ -1650,6 +1650,61 @@ check_strata <- function(strata) {
 }
 
 
+#' Relate two definitions of strata
+#'
+#' Takes two strata definitions (e.g. StrataSpecht and StrataCERMB) and returns
+#' a list where the number of elements is equal to the number of strata in the
+#' first set, and each element is a vector with indices for strata in the
+#' second set.
+#'
+#' @param strata1 A data frame of strata definitions. Usually this should be
+#'   the set with the smaller number of strata.
+#'
+#' @param strata2 A data frame of strata definitions. Usually this should be
+#'   the set with the larger number of strata.
+#'
+#' @return A named list, where element names are taken from \code{strata1}
+#'   and each element is a vector of the indices of the related strata in
+#'   \code{strata2}.
+#'
+#' @export
+#'
+relate_strata <- function(strata1, strata2) {
+  strata1 <- check_strata(strata1)
+  strata2 <- check_strata(strata2)
+
+  n1 <- nrow(strata1)
+  n2 <- nrow(strata2)
+  Eps <- 1e-6
+
+  relations <- vector("list", n1)
+  for (i1 in 1:n1) {
+    lims1 <- c(strata1$lower[i1], strata1$upper[i1])
+    k <- ifelse(i1 == 1, 1, 1 + tail(relations[[i1-1]], 1))
+    ii <- sapply(k:n2, function(i2) {
+      .overlap(lims1, c(strata2$lower[i2] + Eps, strata2$upper[i2]))
+    })
+    relations[[i1]] <- (k-1) + which(ii)
+  }
+
+  names(relations) <- strata1$name
+
+  relations
+}
+
+.within <- function(x, lims) {
+  lims <- sort(lims)
+  x >= lims[1] && x < lims[2]
+}
+
+.overlap <- function(lims1, lims2) {
+  .within(lims1[1], lims2) ||
+    .within(lims1[2], lims2) ||
+    .within(lims2[1], lims1) ||
+    .within(lims2[2], lims1)
+}
+
+
 #' Update a LAS header based on current point data
 #'
 #' This function can be called to update the header information of a LAS object
